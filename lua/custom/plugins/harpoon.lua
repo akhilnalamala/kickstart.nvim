00ed5b09410bfae8082dab58543aaa9d7fe3237c
Akhil Nalamala <akhilnalamala1@gmail.com>
2024-06-13 10:37:17 +0530

    finalize harpoon keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -29,5 +29,33 @@ return {
     vim.keymap.set('n', '<C-e>', function()
       toggle_telescope(harpoon:list())
     end, { desc = 'Open harpoon window' })
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Add Harpoon mark' })
+    vim.keymap.set('n', '<leader>x', function()
+      harpoon:list():remove()
+    end, { desc = 'Remove Harpoon mark' })
+
+    vim.keymap.set('n', '<leader>1', function()
+      harpoon:list():select(1)
+    end, { desc = 'Harpoon to 1st file' })
+    vim.keymap.set('n', '<leader>2', function()
+      harpoon:list():select(2)
+    end, { desc = 'Harpoon to 2nd file' })
+    vim.keymap.set('n', '<leader>3', function()
+      harpoon:list():select(3)
+    end, { desc = 'Harpoon to 3rd file' })
+    vim.keymap.set('n', '<leader>4', function()
+      harpoon:list():select(4)
+    end, { desc = 'Harpoon to 4th file' })
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<M-,>', function()
+      harpoon:list():prev()
+    end, { desc = 'Harpoon to previous buffer' })
+    vim.keymap.set('n', '<M-.>', function()
+      harpoon:list():next()
+    end, { desc = 'Harpoon to next buffer' })
   end,
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,10 +1,18 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-  options = {
-    theme = 'oxocarbon',
-  },
   config = function()
-    require('lualine').setup {}
+    require('lualine').setup {
+      options = {
+        theme = 'oxocarbon',
+      },
+      sections = {
+        lualine_c = { 'filename', 'filesize' },
+      },
+      extensions = {
+        'oil',
+        'nvim-dap-ui',
+      },
+    }
   end,
 }
