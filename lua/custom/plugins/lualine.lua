8cb18ae7430f9bf95763c38ef111a35ca5d628c9
akhilnalamala <akhilnalamala1@gmail.com>
2025-03-17 04:46:14 +0530

    add back custom plugins + clean out custom/plugins folder

--- lua/custom/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
--- lualine replacement for modeline
-return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup()
-  end,
-}
