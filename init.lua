4dab32b33c2aab10289b179c0f889a02afc5f70f
akhilnalamala <akhilnalamala1@gmail.com>
2025-04-17 11:38:37 +0530

    switch to gruvbox

--- init.lua
+++ init.lua
@@ -899,7 +899,8 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'nyoom-engineering/oxocarbon.nvim',
+    'ellisonleao/gruvbox.nvim',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -907,8 +908,10 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.opt.termguicolors = true
       vim.opt.background = 'dark'
-      vim.cmd.colorscheme 'oxocarbon'
+
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 
